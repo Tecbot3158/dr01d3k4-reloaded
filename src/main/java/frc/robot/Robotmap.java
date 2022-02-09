e5b40654a09fd548fb7755d7041878748fe4a176
a <a@example.org>
2022-02-09 12:09:36 -0600

    update ports

--- src/main/java/frc/robot/Robotmap.java
+++ src/main/java/frc/robot/Robotmap.java
@@ -1,10 +1,10 @@
 package frc.robot;
 
 public class Robotmap {
-    public static int leftMotorPort = 2;
-    public static int leftMotorPort1 = 9;
-    public static int rightMotorPort = 1;
-    public static int rightMotorPort1 = 10;
+    public static int leftMotorPort = 13;
+    public static int leftMotorPort1 = 12;
+    public static int rightMotorPort = 3;
+    public static int rightMotorPort1 = 4;
 
 public static int transmissionSolenoidPort1 = 2;
 public static int transmissionSolenoidPort2 = 3;
