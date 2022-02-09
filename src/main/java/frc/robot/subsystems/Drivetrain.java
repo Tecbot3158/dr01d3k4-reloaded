994daedc82cb901c83d8294b455ea22f87e33e94
a <a@example.org>
2022-02-09 12:11:57 -0600

    change motor types to TecbotSpeedController

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,13 +5,21 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robotmap;
+import frc.robot.resources.TecbotSpeedController;
+import frc.robot.resources.TecbotSpeedController.TypeOfMotor;
 
 public class Drivetrain extends SubsystemBase {
 
-    CANSparkMax rightMotorPort ;
-    CANSparkMax rightMotorPort1;
-    CANSparkMax leftMotorPort ;
-    CANSparkMax leftMotorPort1 ;
+    //CANSparkMax rightMotorPort ;
+    //CANSparkMax rightMotorPort1;
+    //CANSparkMax leftMotorPort ;
+    //CANSparkMax leftMotorPort1 ;
+
+
+    TecbotSpeedController rightMotorPort ;
+    TecbotSpeedController rightMotorPort1;//
+    TecbotSpeedController leftMotorPort ;
+    TecbotSpeedController leftMotorPort1 ;
 
     
 public Drivetrain () 
@@ -19,10 +27,23 @@ public Drivetrain ()
 
 
     
-     rightMotorPort = new CANSparkMax(Robotmap.rightMotorPort, MotorType.kBrushless) ;
-     rightMotorPort1 = new CANSparkMax (Robotmap.rightMotorPort1, MotorType.kBrushless);
-     leftMotorPort = new CANSparkMax (Robotmap.leftMotorPort, MotorType. kBrushless);
-     leftMotorPort1 = new CANSparkMax (Robotmap.leftMotorPort1, MotorType. kBrushless);
+     //rightMotorPort = new CANSparkMax(Robotmap.rightMotorPort, MotorType.kBrushless) ;
+     //rightMotorPort1 = new CANSparkMax (Robotmap.rightMotorPort1, MotorType.kBrushless);
+     //leftMotorPort = new CANSparkMax (Robotmap.leftMotorPort, MotorType. kBrushless);
+     //leftMotorPort1 = new CANSparkMax (Robotmap.leftMotorPort1, MotorType. kBrushless);
+
+     
+     rightMotorPort = new TecbotSpeedController(Robotmap.rightMotorPort,  TypeOfMotor.TALON_SRX) ;
+     rightMotorPort1 = new TecbotSpeedController (Robotmap.rightMotorPort1, TypeOfMotor.TALON_SRX);
+     //rightMotorPort.isInverted();
+     //rightMotorPort.isInverted();
+
+
+     leftMotorPort = new TecbotSpeedController (Robotmap.leftMotorPort, TypeOfMotor.TALON_SRX);
+     leftMotorPort1 = new TecbotSpeedController (Robotmap.leftMotorPort1, TypeOfMotor.TALON_SRX);
+
+     //leftMotorPort.isInverted();
+     //leftMotorPort1.isInverted();
 
 }
 public void driveTank(double y, double x)
@@ -30,14 +51,24 @@ public void driveTank(double y, double x)
     // y -> speed
     // x -> turn
 
+//    double leftSpeed = -y + x;
+//    double rightSpeed = -y - x;
+
+    
+
     double leftSpeed = -y + x;
     double rightSpeed = -y - x;
 
     leftMotorPort.set(leftSpeed);
     leftMotorPort1.set(leftSpeed);
 
-    rightMotorPort.set(rightSpeed);
-    rightMotorPort1.set(rightSpeed);
+    rightMotorPort.set(-rightSpeed);
+    rightMotorPort1.set(-rightSpeed);
+
+    System.out.println("x is: " + x);    
+    System.out.println("y is: " + y);
+
+
 } ;
 
 }
